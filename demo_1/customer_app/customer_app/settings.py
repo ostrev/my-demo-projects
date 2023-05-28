f06d787da66fc7ad3b49dc1551bff46fac7e167c
ostrev <npostrev@gmail.com>
2023-05-28 15:13:31 +0300

    add reset password send email

--- demo_1/customer_app/customer_app/settings.py
+++ demo_1/customer_app/customer_app/settings.py
@@ -140,3 +140,13 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'myarielitv@gmail.com'
+EMAIL_HOST_PASSWORD = 'ulirkjyctmxnnjar'
