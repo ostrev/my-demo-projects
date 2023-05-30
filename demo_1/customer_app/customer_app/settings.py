1e1a662cbfc369bf8b34b4acd37079f92b5b2b26
ostrev <npostrev@gmail.com>
2023-05-30 09:06:50 +0300

    last commit

--- demo_1/customer_app/customer_app/settings.py
+++ demo_1/customer_app/customer_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os.path
 from pathlib import Path
 
+from django.contrib import staticfiles
 
 # import sys
 # print(sys.version)
@@ -144,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
